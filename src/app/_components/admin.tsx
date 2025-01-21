c5d997eaccb059b2dd6edeabbf984b387d564442
nick-dev123 <144809451+nick-dev123@users.noreply.github.com>
2025-01-21 20:08:01 +0200

    feat: show users in table

--- src/app/_components/admin.tsx
+++ src/app/_components/admin.tsx
@@ -8,35 +8,72 @@ export function AdminDashboard() {
   const [users] = api.post.getUsers.useSuspenseQuery();
 
   const utils = api.useUtils();
+
+  const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+
   const inviteUser = api.post.addUser.useMutation({
     onSuccess: async () => {
       await utils.post.invalidate();
+      setName("");
       setPhoneNumber("");
     },
   });
 
   return (
-    <div className="w-full max-w-xs">
-      {users && users[0] ? (
-        <p className="truncate">User: {users[0].name}</p>
+    <div className="flex w-full max-w-xs flex-col items-center justify-center gap-4 text-center">
+      {users && users.length ? (
+        <table className="border-collapse border border-slate-500 ...">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 ...">Name</th>
+              <th className="border border-slate-600 ...">Phone Number</th>
+              <th className="border border-slate-600 ...">
+                Authentication Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.phoneNumber}>
+                <td className="border border-slate-700 ...">{user.name}</td>
+                <td className="border border-slate-700 ...">
+                  {user.phoneNumber}
+                </td>
+                <td className="border border-slate-700 ...">
+                  {user.authStatus ? "Completed" : "Sent invite"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ) : (
-        <p>You have no users yet.</p>
+        <p>No users invited yet.</p>
       )}
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          inviteUser.mutate({ phoneNumber });
+          inviteUser.mutate({ name, phoneNumber });
+
         }}
-        className="flex flex-col gap-2"
+        className="flex flex-col gap-4"
       >
-        <input
-          type="text"
-          placeholder="Phone Number"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          className="w-full rounded-full px-4 py-2 text-black"
-        />
+        <div className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full rounded-full px-4 py-2 text-black"
+          />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            className="w-full rounded-full px-4 py-2 text-black"
+          />
+        </div>
         <button
           type="submit"
           className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
