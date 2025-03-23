52c6becb038828899cb76b53862a2578fdad606a
Peter <kompaszbence@gmail.com>
2025-03-23 17:36:40 +0200

    feat: fund task

---
+++ hardhat/tasks/fund.ts
@@ -0,0 +1,20 @@
+import { task, types } from "hardhat/config";
+
+task("fund", "Sends ETH to an address")
+  .addParam("to", "The recipient address")
+  .addOptionalParam("amount", "Amount in ETH", "0.1", types.string)
+  .setAction(async (taskArgs, hre) => {
+    const [deployer] = await hre.ethers.getSigners();
+
+    const tx = await deployer.sendTransaction({
+      to: taskArgs.to,
+      value: hre.ethers.parseEther(taskArgs.amount),
+    });
+
+    console.log(`Sent ${taskArgs.amount} ETH to ${taskArgs.to}`);
+    console.log(`Transaction hash: ${tx.hash}`);
+
+    await tx.wait();
+  });
+
+export default {};
