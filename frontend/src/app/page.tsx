09283c0d5cb508fa9b129b934d38786380e9bd1b
Peter <kompaszbence@gmail.com>
2025-03-24 18:29:21 +0200

    chore: cleanup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,11 @@
 import { api, HydrateClient } from "~/trpc/server";
-import Giveaway from "./give-away/page";
+import Giveaway from "./_components/Giveaway";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">Bulk Pregen Demo</p>
-          </div>
-          <Giveaway></Giveaway>
-        </div>
+      <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <Giveaway></Giveaway>
       </main>
     </HydrateClient>
   );
