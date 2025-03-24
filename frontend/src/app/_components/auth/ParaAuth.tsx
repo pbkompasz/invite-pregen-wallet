9d90d057c5bae653a2fb436e37ea5b11a9b286ae
Peter <kompaszbence@gmail.com>
2025-03-24 18:28:46 +0200

    chore: cleanup

--- frontend/src/app/_components/auth/ParaAuth.tsx
+++ frontend/src/app/_components/auth/ParaAuth.tsx
@@ -4,40 +4,10 @@ import { useState } from "react";
 import para from "~/lib/frontend/para";
 import "@getpara/react-sdk/styles.css";
 import { ParaModal, useAccount, useWallet } from "@getpara/react-sdk";
-import { api } from "~/trpc/react";
 
 export default function AdminAuth() {
   const [showParaLogin, setShowParaLogin] = useState(false);
 
-  const utils = api.useUtils();
-
-  const {
-    data: admin,
-    mutate: createAdmin,
-    isSuccess,
-    isPending,
-  } = api.admin.addAdmin.useMutation({
-    onSuccess: async () => {
-      await utils.admin.invalidate();
-    },
-  });
-
-  const finalizeLogin = async () => {
-    const userId = para.getUserId() as string;
-    const email = para.getEmail() as string;
-    const address = para.getAddress() as string;
-    const phoneNumber = para.getPhoneNumber() as string;
-    const userShare = para.getUserShare() as string;
-    createAdmin({
-      userId,
-      email,
-      userShare,
-      address,
-      phoneNumber,
-    });
-  };
-
-  
   const { data: account } = useAccount();
   const { data: wallet } = useWallet();
 
@@ -69,7 +39,6 @@ export default function AdminAuth() {
         isOpen={showParaLogin}
         appName="ParaPregenDemo"
         disablePhoneLogin={false}
-        // onClose={() => finalizeLogin()}
       />
     </div>
   );
